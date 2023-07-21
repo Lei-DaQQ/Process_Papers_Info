14c2bd6c221d5f8acc61b32bceac4cd532b52979
ljx <ljx.1024@outlook.com>
2023-07-21 21:19:25 +0800

    process .xml to .excel

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Readme
 提取每一个<hit>里author、title、venue、year、ee，如果有多个作者，放在excel里放在一个单元格中.
+
+# Usage
+
+```shell
+pip install -r requires.txt
+```
--- xml2excel.py
+++ xml2excel.py
@@ -1,7 +1,10 @@
 
+import os
 import xml.etree.ElementTree as ET
 import pandas as pd
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 def parse_dblp_xml(xml_file):
     tree = ET.parse(xml_file)
@@ -18,13 +21,29 @@ def parse_dblp_xml(xml_file):
         pub_dict['year'] = info.find('year').text.strip() if info.find('year') is not None else ''
         pub_dict['venue'] = info.find('venue').text.strip() if info.find('venue') is not None else ''
         pub_dict['ee'] = info.find('ee').text.strip() if info.find('ee') is not None else ''
-        
+
+        #if pub_dict['ee']:
+        #    abstract = get_abstract(pub_dict['ee'])
+        #    pub_dict['abstract'] = abstract.strip() if abstract else ''
+        #else:
+        #    pub_dict['abstract'] = ''       
+
         publications.append(pub_dict)
     
     return publications
 
+def get_abstract(url):
+    response = requests.get(url)
+    print(response)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        abstract_tag = soup.find('abstract')
+        if abstract_tag:
+            return abstract_tag.get_text()
+    return ''
 def export_to_excel(data, excel_file):
     df = pd.DataFrame(data)
+    df = df[['title', 'venue', 'year', 'ee', 'authors']]
     df.to_excel(excel_file, index=False)
 
 if __name__ == '__main__':
@@ -32,8 +51,9 @@ if __name__ == '__main__':
         print("Usage: python script_name.py <input.xml>")
     else:
         input_xml_file = sys.argv[1]
-        output_excel_file = 'output.xlsx'
+        output_excel_file = os.path.splitext(input_xml_file)[0] + '.xlsx'
+        #'output.xlsx'
 
         publications_data = parse_dblp_xml(input_xml_file)
         export_to_excel(publications_data, output_excel_file)
-        print(f"Data from '{input_xml_file}' has been successfully exported to 'output.xlsx'.")
+        print(f"Data from '{input_xml_file}' has been successfully exported to '{output_excel_file}'.")
